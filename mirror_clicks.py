435a9640e08c41458e771a22a2c91946c4f7e9a0
vlad-v-melnikov <vmelnikov.miis@gmail.com>
2020-09-25 11:43:03 -0400

    settings are in a class of their own now

--- mirror_clicks.py
+++ mirror_clicks.py
@@ -1,23 +1,10 @@
-from pynput import keyboard, mouse
+from pynput import keyboard
 import pyautogui, logging, json
 from modules.compare import compare_images
+from settings import Settings
 
 ###################################################
-try:
-    with open('settings.json') as f:
-        settings = json.load(f)
-    keymap_exit = settings['keymap_exit']
-    keymap_action = settings['keymap_action']
-    keymap_compare = settings['keymap_compare']
-    screenshot_region = tuple(settings['screenshot_region'])
-    compare_region = tuple(settings['compare_region'])
-    confidence_level = settings['confidence_level']
-
-except FileNotFoundError:
-    print("settings.json file not found. Quitting the script...")
-    exit()
-
-screen_width, screen_height = pyautogui.size()
+settings = Settings()
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 ###################################################
@@ -28,11 +15,11 @@ def jump_mouse():
     print("Click", end='... ')
     
     pos_x, pos_y = pyautogui.position()
-    pyautogui.click(x=pos_x + screen_width/2, y=pos_y, button='left')
+    pyautogui.click(x=pos_x + settings.screen_width/2, y=pos_y, button='left')
     logging.debug("Clicking at {}".format(pyautogui.position()))
     print("Move... Click", end='...')
     
-    pyautogui.moveRel(-screen_width/2, 0)
+    pyautogui.moveRel(-settings.screen_width/2, 0)
     logging.debug("Moved back to {}".format(pyautogui.position()))
 
     print("Move back.")
@@ -41,20 +28,21 @@ def on_press(key):
     vk = key.vk if hasattr(key, 'vk') else key.value.vk
     logging.debug('key pressed = ', vk)
 
-    if(vk in keymap_exit.values()):
+    if(vk in settings.keymap_exit.values()):
         return False
     
-    if(vk in keymap_action.values()):
+    if(vk in settings.keymap_action.values()):
         jump_mouse()
-    elif(vk in keymap_compare.values()):
-        compare_images(screenshot_region, compare_region, confidence_level)
+    elif(vk in settings.keymap_compare.values()):
+        compare_images(settings.screenshot_region, settings.compare_region, settings.confidence_level)
     else:
         print(f'Key {vk} is not bound.')
 
+###################################################
+print("Starting...")
+print("Press {} to click/mirror click.".format(settings.keymap_action.keys()))
+print("Press {} to compare images.".format(settings.keymap_compare.keys()))
+print("Press {} to quit.".format(settings.keymap_exit.keys()))
 with keyboard.Listener(on_press=on_press) as listener:
-    print("Starting...")
-    print("Press {} to click/mirror click.".format(keymap_action.keys()))
-    print("Press {} to compare images.".format(keymap_compare.keys()))
-    print("Press {} to quit.".format(keymap_exit.keys()))
     listener.join()
     
