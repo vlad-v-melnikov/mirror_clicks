e8c4fadccdda314fc036b279f276d84178663202
vlad-v-melnikov <vmelnikov.miis@gmail.com>
2020-09-25 14:18:32 -0400

    Added check for screenshot dir

--- modules/compare.py
+++ modules/compare.py
@@ -1,7 +1,11 @@
-import pyautogui
+import pyautogui, os
 
 def compare_images(screenshot_region, compare_region, confidence_level=0.95):
     print("Comparing images...")
+
+    if not os.path.isdir('./screenshot'):
+        os.mkdir('./screenshot')
+
     pyautogui.screenshot('screenshot\\left.png', region=screenshot_region)
     result = pyautogui.locateOnScreen('screenshot\\left.png', \
         region=compare_region,\
