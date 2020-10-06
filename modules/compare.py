19de880ff7eab356a73e0cba6d32841c32e3d368
vlad-v-melnikov <vmelnikov.miis@gmail.com>
2020-10-06 09:20:48 -0400

    Formatting compliance.

--- modules/compare.py
+++ modules/compare.py
@@ -8,9 +8,9 @@ def compare_images(screenshot_region, compare_region, confidence_level=0.95):
         os.mkdir('./screenshot')
 
     pyautogui.screenshot('screenshot\\left.png', region=screenshot_region)
-    result = pyautogui.locateOnScreen('screenshot\\left.png', \
-        region=compare_region,\
-        confidence=confidence_level)
+    result = pyautogui.locateOnScreen('screenshot\\left.png',
+                                      region=compare_region,
+                                      confidence=confidence_level)
 
     if result:
         print('Images match')
